3195e45e70ba02f4bbc4b9eea83e9f11b32f5b27
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-12 16:00:58 +0530

    shell-print

--- 02-print.sh
+++ 02-print.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo " Zeeshan"
+echo " SHELL SCRIPTING TOPICS"
 
-#echo is a keyword used to print statements
+#echo is a keyword used to print a msg on the screen
 
-echo  -e "Zeeshan \n hyder "
+echo  -e "01-SHEBANG  \n 02-PRINT \n 03-Variable \n 04-Local Variables "
+
+# Here -e "enable escape sequence"
+# \n used for line by line
