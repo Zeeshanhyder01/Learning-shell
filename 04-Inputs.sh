918a8e56365c8e424d9625c46fef430d78cd53ea
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-12 20:56:34 +0530

    shell-Read Inputs

--- 04-Inputs.sh
+++ 04-Inputs.sh
@@ -4,6 +4,6 @@ read -p ' Enter Your name :' Name
 
 echo -e " Hey $Name  Welcome to the Devops classes"
 
-read -p ' Please enter your contact :' contat
+read -p "Please enter your contact :" contat
 
 echo -e "\e[32m Thank for sharing ur number $contact You \e[0"
