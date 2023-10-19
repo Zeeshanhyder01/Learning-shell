245d7869c527bb4da8b9c8234a48834bf9e76568
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-10-19 20:06:15 +0530

    shell script-exit status|command

--- 06-exit.sh
+++ 06-exit.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-ls
-echo hello
-exit 0
+#ls
+#echo hello
+#exit 0
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]
+echo you are non root user
+echo you can run this scrpit as a root user or use sudo
+exit 1
+fi
+
+
+
